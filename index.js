a0d9cd5065f8b8e626bd4fb4f6eaa6a0fc83ad8e
ashiful islam <ashifulislam2002@gmail.com>
2025-06-15 23:49:49 +0600

    implement the basic routes

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
+const admin = require("firebase-admin");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = process.env.MONGODB_URI;
 app.use(express.json());
 app.use(cors());
+
+const varifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Unauthorizes" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = await admin.auth().varifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,18 +43,70 @@ async function run() {
     const eventsCollection = client.db("eventApp").collection("events");
     const joinedCollection = client.db("eventApp").collection("joinedEvents");
 
-    // get jobs
+    // get events
+    // app.get("/events", async (req, res) => {
+    //   // const userEmail = req.user.email;
+    //   const result = await eventsCollection.find().toArray();
+    //   // console.log("user Email from middleware", userEmail);
+    //   res.send(result);
+    // });
+    // get events
     app.get("/events", async (req, res) => {
-      const result = await eventsCollection.find().toArray();
+      const { type, search } = req.query;
+      const todayISOString = new Date().toISOString();
+      const filter = {
+        date: { $gt: todayISOString },
+      };
+      // const filter = {};
+      if (type) {
+        filter.eventType = type;
+      }
+      if (search) {
+        filter.title = {
+          $regex: search,
+          $options: "i",
+        };
+      }
+      const result = await eventsCollection
+        .find(filter)
+        .sort({ date: 1 })
+        .toArray();
       res.send(result);
     });
+
     // get single event
     app.get("/events/:id", async (req, res) => {
       const id = req.params.id;
       const result = await eventsCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
+    //patch to update event
+    app.put("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          title: updatedData.title,
+          location: updatedData.location,
+          eventType: updatedData.eventType,
+          date: updatedData.date,
+          thumbnail: updatedData.thumbnail,
+          description: updatedData.description,
+        },
+      };
+      const result = await eventsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
+    // delete event
+    app.use("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await eventsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
     // join an event
     app.patch("/joined-events", async (req, res) => {
       const joinData = req.body;
@@ -64,6 +132,7 @@ async function run() {
       const email = req.params.email;
       const result = await joinedCollection
         .find({ userEmail: email })
+        .sort({ date: 1 })
         .toArray();
       res.send(result);
     });
@@ -79,12 +148,18 @@ async function run() {
       res.send({ joined: !!joined });
     });
 
-    // post jobs
+    // post events
     app.post("/events", async (req, res) => {
       const eventData = req.body;
       console.log(eventData);
       const result = await eventsCollection.insertOne(eventData);
       res.send(result);
+    });
+
+    // update events
+
+    app.patch("/update-event/:id", async (req, res) => {
+      const id = req.body.id;
     });
 
     await client.db("admin").command({ ping: 1 });
